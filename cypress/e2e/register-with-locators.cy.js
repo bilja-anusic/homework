9c197b1e1dad85576398b491510ebdd6ff0c4212
bilja-anusic <bilja.anusic@gmail.com>
2022-12-14 19:26:03 +0100

    finished homework

--- cypress/e2e/register-with-locators.cy.js
+++ cypress/e2e/register-with-locators.cy.js
@@ -21,4 +21,60 @@ describe("Register page test", ()=>{
         cy.get(locators.register.button).click()
     
     })
+
+    it ("Register without first name",() =>{
+
+        const password = faker.internet.password()
+        
+        cy.get(locators.register.lastName).type(faker.name.lastName())
+        cy.get(locators.register.email).type(faker.internet.email())
+        cy.get(locators.register.password).type(password)
+        cy.get(locators.register.passwordConfirmation).type(password)
+        cy.get(locators.register.terms).click()
+        cy.get(locators.register.button).click()
+    
+    })
+
+    it ("Register without last name",() =>{
+
+        const password = faker.internet.password()
+        
+        cy.get(locators.register.firstName).type(faker.name.firstName())
+        cy.get(locators.register.email).type(faker.internet.email())
+        cy.get(locators.register.password).type(password)
+        cy.get(locators.register.passwordConfirmation).type(password)
+        cy.get(locators.register.terms).click()
+        cy.get(locators.register.button).click()
+
+
+    })
+
+    it ("Register with invalid email",() =>{
+
+    const password = faker.internet.password()
+    
+    cy.get(locators.register.firstName).type(faker.name.firstName())
+    cy.get(locators.register.lastName).type(faker.name.lastName())
+    cy.get(locators.register.email).type("bilja@@yahoo.com")
+    cy.get(locators.register.password).type(password)
+    cy.get(locators.register.passwordConfirmation).type(password)
+    cy.get(locators.register.terms).click()
+    cy.get(locators.register.button).click()
+
+    })
+
+    it ("Register with invalid password confirmations",() =>{
+
+    const password = faker.internet.password()
+    
+    cy.get(locators.register.firstName).type(faker.name.firstName())
+    cy.get(locators.register.lastName).type(faker.name.lastName())
+    cy.get(locators.register.email).type(faker.internet.email())
+    cy.get(locators.register.password).type(password)
+    cy.get(locators.register.passwordConfirmation).type("12345678")
+    cy.get(locators.register.terms).click()
+    cy.get(locators.register.button).click()
+
+    })
+
 })
